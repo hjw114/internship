90589187d108aeccf6c274daa719d5afc05ee6ac
q66935799 <2444717994@qq.com>
2020-06-28 17:51:53 +0800

    Reverse string

--- Hand training procedure/Reverse string.py
+++ Hand training procedure/Reverse string.py
@@ -1,5 +1,5 @@
-Str1=input("请输入字符串:")
-Str2=list(Str1)
-Str2.reverse()
-Str=''.join(Str2)
-print(Str)
+Str1=input("请输入字符串:")#输入字符串
+Str2=list(Str1)#类型转换list
+Str2.reverse()#逆序
+Str=''.join(Str2)#类型转换str
+print(Str)#输出
