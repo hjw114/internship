07f3e4a781dfe60d227292a00eb590eb5e86e93f
sqs0118 <67545255+sqs0118@users.noreply.github.com>
2020-07-07 15:39:19 +0800

    sqlite

---
+++ cxsj_yhsrdz.py
@@ -0,0 +1,34 @@
+#coding=gbk
+#查询数据
+import sqlite3 
+DB_Name = input('请输入你想打开的数据库的路径:')  # 例如D:\sqlite\test.db 
+
+# 连接数据库，如果不存在则会在当前目录创建
+conn = sqlite3.connect(DB_Name)
+try:    
+	# 创建游标    
+	cursor = conn.cursor()    
+	
+	# 查询数据的SQL语句    
+	SQL = '''          
+		SELECT * FROM STUDENT;          
+		'''    
+		
+	# 查询数据    
+	cursor.execute(SQL)     
+	
+	# 获取一条数据    
+	one = cursor.fetchone()    
+	print(one)     
+	
+	# 获取所有数据    
+	for row in cursor.fetchall():        
+		print(row) 
+		
+except Exception as e:    
+	print(e)    
+	print('查询数据失败')
+	
+finally:    
+	# 关闭数据库    
+	conn.close()
