a64d22f915fb54b175e5ae5959d114e2f36b3ee2
sqs0118 <67545255+sqs0118@users.noreply.github.com>
2020-07-07 15:46:53 +0800

    电子卡片生成器1.0
    目前可以实现的功能：可以选择本地一张图片，在上面添加姓名、生日、名言警句，，但不能输入中文
    添加文本的位置可以在程序中修改，但不能在输入端控制

---
+++ Hand training procedure/dzkpscq.py
@@ -0,0 +1,29 @@
+#coding=gbk
+from PIL import Image,ImageDraw
+ 
+def addText(img,string):
+  size = img.size
+  width = size[0] - 50
+  high = size[1] - 60
+  lenth = len(string)*5
+  draw = ImageDraw.Draw(img)
+  draw.text((width-lenth,high),string,fill='black')
+  oriImg.show()
+  oriImg.save(path)
+ 
+ 
+path = input("Please input the image file with path")
+
+string1=input('对不起qaq，本程序目前不能接受中文'+'\n'+'请输入你的姓名：')
+string2=input('\n'+'请输入你的生日：')
+string3=input('\n'+'请输入你的格言：')
+string=string1+'\n'+string2+'\n'+string3
+ 
+try:
+  print("path: "+path)
+  oriImg = Image.open(path)
+  addText(oriImg,string)
+except IOError:
+  print("can't' open the file,check the path again")
+  newImg = Image.new('RGBA',(320,240),'white')
+  newImg.save(path)
